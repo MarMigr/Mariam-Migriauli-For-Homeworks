157333f5de61943a8c2b537e45fb996d00424803
MarMigr <mariammigriauli16@gmail.com>
2024-09-13 13:53:54 +0400

    this is from lesson1 but in folder

---
+++ homework_1/Task1.py
@@ -0,0 +1,6 @@
+print('Please Fill Your Information')
+name=input('Name: ') 
+surname=input('Surname: ')  
+age=input('Age: ')
+
+print('Hello',name,surname,'you are',age, 'years old')
---
+++ homework_1/Task2.py
@@ -0,0 +1,6 @@
+name=str(input('Your Name :' ))
+birthday_year=int(input('Your Birth Year:' ))
+current_year=2024
+
+
+print("Hello",name,'You Are ',current_year-birthday_year, 'old')
